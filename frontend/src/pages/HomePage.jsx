8abb849615b0bb654a2b71fb79e4b4134f7a10c9
Art San <artsan.mcc@gmail.com>
2024-02-23 23:52:06 +0700

    ok

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,61 @@
+import { useState, useEffect, useCallback } from 'react'
 import ProfileInfo from '../components/ProfileInfo'
 import Repos from '../components/Repos'
 import Search from '../components/Search'
 import SortRepos from '../components/SortRepos'
-// import Spinner from '../components/Spinner'
+
+import toast from 'react-hot-toast'
+import Spinner from '../components/Spinner'
 const HomePage = () => {
+  const [userProfile, setUserProfile] = useState(null)
+  const [repos, setRepos] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  // const [sortType, setSortType] = useState('recent')
+  // console.log('userProfile', userProfile)
+  // console.log('repos', repos)
+  // console.log('loading', loading)
+
+  const getUserProfileAndRepos = useCallback(async () => {
+    setLoading(true)
+
+    try {
+      const userRes = await fetch('https://api.github.com/users/Art-San')
+
+      const userProfile = await userRes.json()
+      setUserProfile(userProfile)
+
+      const repoRes = await fetch(userProfile.repos_url)
+      const repos = await repoRes.json()
+
+      // repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)) //descending, recent first
+
+      setRepos(repos)
+      console.log('userProfile', userProfile)
+      console.log('repos', repos)
+
+      return { userProfile, repos }
+    } catch (error) {
+      toast.error(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    getUserProfileAndRepos()
+  }, [getUserProfileAndRepos])
+
   return (
     <div className="m-4">
       <Search />
       <SortRepos />
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-        <ProfileInfo />
-        <Repos />
-        {/* <Spinner // 1:02:00
-        /> */}
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+        {/* <ProfileInfo userProfile={userProfile} /> */}
+
+        {repos.length > 0 && !loading && <Repos repos={repos} />}
+        {loading && <Spinner />}
       </div>
     </div>
   )
